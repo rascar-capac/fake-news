aa813c4eda1c100e5970f5225baedc46bb636049
rascar-capac <to.constant@gmail.com>
2020-04-20 14:14:04 +0200

    Add random contamination variation

--- Assets/Scripts/PopulationHandler.cs
+++ Assets/Scripts/PopulationHandler.cs
@@ -8,11 +8,16 @@ public class PopulationHandler : MonoBehaviour
     [SerializeField] [Range(0, int.MaxValue)] private int startingParanoiaLevel = 0;
     [SerializeField] [Range(0, int.MaxValue)] private int startingContaminationLevel = 0;
     [SerializeField] [Range(0, int.MaxValue)] private int startingCasualtiesCount = 0;
+    [SerializeField] [Range(0, 1f)] private float randomContaminationVariationProbability = 0.1f;
+    [SerializeField] [Range(0, 10f)] private float tickPeriod = 0.5f;
 
     public int TrustLevel { get; set; }
     public int ParanoiaLevel { get; set; }
     public int ContaminationLevel { get; set; }
     public int CasualtiesCount { get; set; }
+
+
+    private float timer;
 
     private void Start()
     {
@@ -20,5 +25,25 @@ public class PopulationHandler : MonoBehaviour
         ParanoiaLevel = startingParanoiaLevel;
         ContaminationLevel = startingContaminationLevel;
         CasualtiesCount = startingCasualtiesCount;
+
+        timer = tickPeriod;
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            if(Random.value <= randomContaminationVariationProbability)
+            {
+                ApplyRandomContaminationVariation();
+            }
+            timer = tickPeriod;
+        }
+    }
+
+    private void ApplyRandomContaminationVariation()
+    {
+        ContaminationLevel += Random.Range(1, 10);
     }
 }
